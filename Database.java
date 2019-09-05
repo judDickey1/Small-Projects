725379d22a99321e39ed0654b874bdd9cdd9df86
judDickey1 <judson.dickey@gmail.com>
2019-09-05 16:37:00 -0500

    Initial Commit of Student Database
    Student Database project to simulate enrolling in University

---
+++ Database.java
@@ -0,0 +1,12 @@
+package studentDatabase;
+
+public class Database {
+
+	public static void main(String[] args) {
+		// Ask how many new users we want to add
+		Student st1 = new Student();
+		//Create new students
+		
+	}
+
+}
